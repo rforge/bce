28b844f39a7dada84e2e80ba5b3e2bb062082363
karlines <karlines@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-04-21 10:03:00 +0000

    added bceOutput, generated with bceInput added pairs example updated help files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bce@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/BCE/R/pairsbce.r
@@ -0,0 +1,32 @@
+pairs.bce <- function(X,
+                      sample=1,
+                      gap=0,
+                      upper.panel = NA,
+                      diag.panel = NA,
+                      ...)
+{
+panel.cor <- function(x, y) text(x=mean(range(x)),y=mean(range(y)),
+                                 labels=format(cor(x,y),digits=2))
+
+panel.hist <- function(x,...) {
+        usr <- par("usr")
+        on.exit(par(usr))
+        par(usr = c(usr[1:2], 0, 2))
+        h <- hist(x, plot = FALSE)
+        breaks <- h$breaks
+        nB <- length(breaks)
+        y <- h$counts
+        y <- y/max(y)
+        rect(breaks[-nB], 0, breaks[-1], y, col = "cyan")
+
+    }
+if (!is.null(upper.panel) && is.na(upper.panel))upper.panel <- panel.cor
+if (!is.null(diag.panel) && is.na(diag.panel))diag.panel <- panel.hist
+
+ifelse("bce"%in%class(X),x <- t(X$X[sample,,]),x <- t(X[sample,,]))
+labels <- colnames(x)
+pairs(x, diag.panel =diag.panel, labels = labels,
+      gap = gap, upper.panel = upper.panel,...)
+
+}
+
